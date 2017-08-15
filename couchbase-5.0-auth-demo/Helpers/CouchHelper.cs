2f9a26623717a2b1a79a7e7ee2e45751e55e4238
Mike Goldsmith <goldsmith.mike@gmail.com>
2017-08-15 10:56:01 +0100

    demo project

---
+++ couchbase-5.0-auth-demo/Helpers/CouchHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+using Couchbase;
+using Couchbase.Authentication;
+using Couchbase.Core;
+
+namespace couchbase_5._0_auth_demo.Helpers
+{
+    public static class CouchHelper
+    {
+        private static readonly Lazy<Cluster> Cluster = new Lazy<Cluster>(SetupCluster);
+
+        private static Cluster SetupCluster()
+        {
+            // create cluster from config
+            var cluster = new Cluster("couchbaseClients/couchbase");
+
+            // get username and password from config then authenticate
+            var username = ConfigurationManager.AppSettings.Get("couchbase_username");
+            var password = ConfigurationManager.AppSettings.Get("couchbase_password");
+            cluster.Authenticate(new PasswordAuthenticator(username, password));
+
+            return cluster;
+        }
+
+        public static IBucket GetBucket(string bucketName)
+        {
+            return Cluster.Value.OpenBucket(bucketName);
+        }
+    }
+}
---
+++ couchbase-5.0-auth-demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace couchbase_5._0_auth_demo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var bucket = Helpers.CouchHelper.GetBucket("default");
+
+            // simulate some traffic
+            Parallel.For(1, 100, index =>
+            {
+                var key = $"key-{index}";
+
+                var upsertResult = bucket.Upsert(key, new {index});
+                if (!upsertResult.Success)
+                {
+                    Console.WriteLine($"Failed to write key {key}");
+                }
+
+                var getResult = bucket.Get<dynamic>(key);
+                if (!getResult.Success)
+                {
+                    Console.WriteLine($"Failed to gey key {key}");
+                }
+            });
+        }
+    }
+}
